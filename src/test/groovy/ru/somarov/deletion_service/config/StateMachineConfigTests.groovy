f00ccb87cb170a4e8ceabbdaf725cc79d8ab3eff
Alex Omarov <33569929+AlexOmarov@users.noreply.github.com>
2022-04-01 14:43:08 +0700

    Refactoring

--- src/test/groovy/ru/somarov/deletion_service/config/StateMachineConfigTests.groovy
+++ src/test/groovy/ru/somarov/deletion_service/config/StateMachineConfigTests.groovy
@@ -3,120 +3,91 @@ package ru.somarov.deletion_service.config
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.context.annotation.Import
 import org.springframework.messaging.support.GenericMessage
 import org.springframework.statemachine.StateMachine
 import org.springframework.statemachine.config.StateMachineFactory
+import org.springframework.statemachine.support.DefaultStateMachineContext
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import reactor.core.publisher.Mono
 import ru.somarov.deletion_service.Application
-import ru.somarov.deletion_service.conf.TestDataSourceConfiguration
 import ru.somarov.deletion_service.constant.SmEvent
-import ru.somarov.deletion_service.domain.entity.Stage
+import ru.somarov.deletion_service.constant.SmState
 import ru.somarov.deletion_service.service.TransitionService
 import spock.lang.Specification
 
 import static org.mockito.ArgumentMatchers.any
 import static org.mockito.Mockito.times
 import static org.mockito.Mockito.verify
-import static ru.somarov.deletion_service.util.TestUtils.init
 
-@Import(TestDataSourceConfiguration)
 @ActiveProfiles(profiles = ["test"])
 @ContextConfiguration(classes = [Application])
 @SpringBootTest
 class StateMachineConfigTests extends Specification {
 
     @Autowired
-    StateMachineFactory<Stage.Code, SmEvent> smFactory
+    StateMachineFactory<SmState, SmEvent> smFactory
 
     @MockBean
     TransitionService service
 
     def "Spring state machine is created in status STARTED"() {
         when:
-        StateMachine<Stage.Code, SmEvent> sm = smFactory.getStateMachine("processId")
+        StateMachine<SmState, SmEvent> sm = smFactory.getStateMachine("processId")
         sm.startReactively().subscribe()
         then:
-        sm.getState().getId() == Stage.Code.STARTED
+        sm.getState().getId() == SmState.STARTED
     }
 
     def "Spring state machine transits from STARTED to WEB_REQUEST_STAGE due to WEB_REQUEST_STARTED event"() {
         setup:
-        StateMachine<Stage.Code, SmEvent> sm = smFactory.getStateMachine("processId")
+        StateMachine<SmState, SmEvent> sm = smFactory.getStateMachine("processId")
         sm.startReactively().subscribe()
         when:
-        sm.sendEvent(Mono.just(new GenericMessage<SmEvent>(SmEvent.WEB_REQUEST_STARTED)))
+        sm.sendEvent(Mono.just(new GenericMessage<SmEvent>(SmEvent.PROCESS_STARTED)))
                 .doOnComplete(result -> println("Mono completed!"))
                 .subscribe()
         then:
         verify(service, times(1)).transit(any())
-        sm.getState().getId() == Stage.Code.WEB_REQUEST_STAGE
+        sm.getState().getId() == SmState.IN_PROGRESS
     }
 
     def "Spring state machine transits from CHECKING to ASYNC_STAGE due to WEB_REQUEST_SUCCEEDED event"() {
         setup:
-        StateMachine<Stage.Code, SmEvent> sm = smFactory.getStateMachine("processId")
-        init(Stage.Code.WEB_REQUEST_STAGE, sm)
+        StateMachine<SmState, SmEvent> sm = smFactory.getStateMachine("processId")
+        init(SmState.IN_PROGRESS, sm)
         when:
-        sm.sendEvent(Mono.just(new GenericMessage<SmEvent>(SmEvent.WEB_REQUEST_SUCCEEDED)))
+        sm.sendEvent(Mono.just(new GenericMessage<SmEvent>(SmEvent.PROCESS_SUCCEEDED)))
                 .doOnComplete(result -> println("Mono completed!"))
                 .subscribe()
         then:
-        verify(service, times(1)).transit(any())
-        sm.getState().getId() == Stage.Code.ASYNC_STAGE
+        verify(service, times(1)).complete(any())
+        sm.getState().getId() == SmState.COMPLETED
     }
 
     def "Spring state machine transits from CHECKING to COMPLETED due to WEB_REQUEST_REJECTED event"() {
         setup:
-        StateMachine<Stage.Code, SmEvent> sm = smFactory.getStateMachine("processId")
-        init(Stage.Code.WEB_REQUEST_STAGE, sm)
+        StateMachine<SmState, SmEvent> sm = smFactory.getStateMachine("processId")
+        init(SmState.IN_PROGRESS, sm)
         when:
-        sm.sendEvent(Mono.just(new GenericMessage<SmEvent>(SmEvent.WEB_REQUEST_REJECTED)))
+        sm.sendEvent(Mono.just(new GenericMessage<SmEvent>(SmEvent.PROCESS_FAILED)))
                 .doOnComplete(result -> println("Mono completed!"))
                 .subscribe()
         then:
         verify(service, times(1)).complete(any())
-        sm.getState().getId() == Stage.Code.COMPLETED
+        sm.getState().getId() == SmState.COMPLETED
     }
 
-    def "Spring state machine transits from CHECKING to COMPLETED due to WEB_REQUEST_FAILED event"() {
-        setup:
-        StateMachine<Stage.Code, SmEvent> sm = smFactory.getStateMachine("processId")
-        init(Stage.Code.WEB_REQUEST_STAGE, sm)
-        when:
-        sm.sendEvent(Mono.just(new GenericMessage<SmEvent>(SmEvent.WEB_REQUEST_FAILED)))
-                .doOnComplete(result -> println("Mono completed!"))
-                .subscribe()
-        then:
-        verify(service, times(1)).complete(any())
-        sm.getState().getId() == Stage.Code.COMPLETED
+    private void init(SmState initState, StateMachine<SmState, SmEvent> sm) {
+        sm.stopReactively().subscribe()
+
+        sm.getStateMachineAccessor()
+                .doWithAllRegions(sma -> sma.resetStateMachineReactively(
+                        new DefaultStateMachineContext<>(initState, null, null, null, null, "1")
+                ).subscribe())
+
+        sm.startReactively().subscribe()
     }
 
-    def "Spring state machine transits from ASYNC_STAGE to COMPLETED due to ASYNC_SUCCEEDED event"() {
-        setup:
-        StateMachine<Stage.Code, SmEvent> sm = smFactory.getStateMachine("processId")
-        init(Stage.Code.ASYNC_STAGE, sm)
-        when:
-        sm.sendEvent(Mono.just(new GenericMessage<SmEvent>(SmEvent.ASYNC_SUCCEEDED)))
-                .doOnComplete(result -> println("Mono completed!"))
-                .subscribe()
-        then:
-        verify(service, times(1)).transit(any())
-        sm.getState().getId() == Stage.Code.COMPLETED
-    }
 
-    def "Spring state machine transits from ASYNC_STAGE to COMPLETED due to ASYNC_ERROR event"() {
-        setup:
-        StateMachine<Stage.Code, SmEvent> sm = smFactory.getStateMachine("processId")
-        init(Stage.Code.ASYNC_STAGE, sm)
-        when:
-        sm.sendEvent(Mono.just(new GenericMessage<SmEvent>(SmEvent.ASYNC_ERROR)))
-                .doOnComplete(result -> println("Mono completed!"))
-                .subscribe()
-        then:
-        verify(service, times(1)).complete(any())
-        sm.getState().getId() == Stage.Code.COMPLETED
-    }
 }
--- src/test/groovy/ru/somarov/deletion_service/service/state_machine/StateMachineServiceTests.groovy
+++ src/test/groovy/ru/somarov/deletion_service/service/state_machine/StateMachineServiceTests.groovy
@@ -1,33 +1,22 @@
 package ru.somarov.deletion_service.service.state_machine
 
-import ru.somarov.deletion_service.Application
-import ru.somarov.deletion_service.conf.TestDataSourceConfiguration
-import ru.somarov.deletion_service.constant.SmEvent
-import ru.somarov.deletion_service.domain.entity.*
-import ru.somarov.deletion_service.domain.repository.Dao
-import ru.somarov.deletion_service.service.StateMachineService
-import ru.somarov.deletion_service.service.TransitionService
-import ru.somarov.deletion_service.service.persister.TransitionPersistenceService
-import org.apache.camel.test.spring.UseAdviceWith
 import org.mockito.ArgumentCaptor
 import org.mockito.InjectMocks
-import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.boot.test.mock.mockito.SpyBean
-import org.springframework.context.annotation.Import
-import org.springframework.statemachine.ExtendedState
 import org.springframework.statemachine.StateContext
 import org.springframework.statemachine.config.StateMachineFactory
-import org.springframework.statemachine.support.DefaultExtendedState
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
+import ru.somarov.deletion_service.Application
+import ru.somarov.deletion_service.constant.SmEvent
+import ru.somarov.deletion_service.constant.SmState
+import ru.somarov.deletion_service.service.StateMachineService
+import ru.somarov.deletion_service.service.TransitionService
 import spock.lang.Specification
 
-import java.time.LocalDateTime
-
-import static org.mockito.ArgumentMatchers.anyLong
 import static org.mockito.ArgumentMatchers.anyString
 import static org.mockito.Mockito.*
 
@@ -41,72 +30,29 @@ class StateMachineServiceTests extends Specification {
     StateMachineService service
 
     @SpyBean
-    StateMachineFactory<Stage.Code, SmEvent> factory
+    StateMachineFactory<SmState, SmEvent> factory
 
     @MockBean
     TransitionService transitionService
 
-    @MockBean
-    TransitionPersistenceService persister
 
-    @MockBean
-    Dao dao
-
-    def "start method builds state machine above deletion process and sends first event to it"() {
+    def "Send method sends event to SM and calls transition service"() {
         setup:
-        long id = 1L
-        SmEvent event = SmEvent.WEB_REQUEST_STARTED
-        ArgumentCaptor<StateContext<Stage.Code, SmEvent>> captor = ArgumentCaptor.forClass(StateContext<Stage.Code, SmEvent>.class)
-        def sm = spy(factory.getStateMachine("1"))
+        SmState state = SmState.STARTED
+        SmEvent event = SmEvent.PROCESS_STARTED
+        ArgumentCaptor<StateContext<SmState, SmEvent>> captor = ArgumentCaptor.forClass(StateContext<SmState, SmEvent>.class)
+        def id = UUID.randomUUID()
+        def sm = spy(factory.getStateMachine(id.toString()))
         reset(factory)
         doReturn(sm).when(factory).getStateMachine(anyString())
-        doReturn(DeletionProcess.builder()
-                .id(1L)
-                .client(Client.builder().id(1L).build())
-                .stage(Stage.builder().code(Stage.Code.STARTED.name()).build())
-                .updated(LocalDateTime.now())
-                .build()).when(dao).startDeletionProcess(anyLong(), Mockito.any())
         when:
-        service.start(id, event, "DEFAULT")
+        service.send(service.build(id, state), event)
         then:
         verify(factory, times(1)).getStateMachine(anyString())        || true
         verify(transitionService, times(1)).transit(captor.capture()) || true
         def context = captor.getValue()
         context.getStateMachine().getId() == sm.getId()
-        context.getTarget().getId() == Stage.Code.WEB_REQUEST_STAGE
+        context.getTarget().getId() == SmState.IN_PROGRESS
         context.getEvent() == event
-    }
-
-    def "sendNextEvent sends event to state machine and sends next event in case it is needed"() {
-        setup:
-        def sm = spy(factory.getStateMachine("1"))
-        reset(factory)
-        ExtendedState state = spy(new DefaultExtendedState())
-        when(state.get("next", SmEvent.class)).thenReturn(SmEvent.WEB_REQUEST_SUCCEEDED, null)
-        when(sm.getExtendedState()).thenReturn(state)
-        doReturn(sm).when(factory).getStateMachine(anyString())
-        def process = DeletionProcess.builder()
-                .id(1L)
-                .client(Client.builder().id(1L).build())
-                .stage(Stage.builder().code(Stage.Code.STARTED.name()).build())
-                .updated(LocalDateTime.now())
-                .build()
-        when(state.get("process", DeletionProcess.class)).thenReturn(process)
-        SmEvent event = SmEvent.WEB_REQUEST_STARTED
-
-        ArgumentCaptor<StateContext<Stage.Code, SmEvent>> captor = ArgumentCaptor.forClass(StateContext<Stage.Code, SmEvent>.class)
-        when:
-        service.send(process, event)
-        then:
-        verify(factory, times(2)).getStateMachine(anyString())        || true
-        verify(transitionService, times(1)).transit(captor.capture()) || true
-        def context = captor.getAllValues()
-        context[0].getStateMachine().getId() == sm.getId()
-        context[0].getTarget().getId() == Stage.Code.ASYNC_STAGE
-        context[0].getEvent() == event
-
-        context[1].getStateMachine().getId() == sm.getId()
-        context[1].getTarget().getId() == Stage.Code.WEB_REQUEST_STAGE
-        context[1].getEvent() == SmEvent.WEB_REQUEST_SUCCEEDED
     }
 }
